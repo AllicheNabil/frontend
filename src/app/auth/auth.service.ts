fe22f35d44f55b0ecc748195443cdcfc8e21245b
allichenabil <allichenabilpediatric@gmail.com>
2025-08-05 11:34:39 +0100

    initial

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,17 +4,22 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { jwtDecode } from 'jwt-decode';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3001/api/auth';
+  private apiUrl = `${environment.apiUrl}/api/auth`;
+
   private _isAuthenticated = false;
   private authToken: string | null = null;
   private userId: number | null = null;
 
-  constructor(private http: HttpClient, @Inject(PLATFORM_ID) private platformId: Object) {
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
     if (isPlatformBrowser(this.platformId)) {
       this.authToken = localStorage.getItem('token');
       this._isAuthenticated = !!this.authToken;
@@ -30,7 +35,12 @@ export class AuthService {
   }
 
   get token(): string | null {
-    return this.authToken || (isPlatformBrowser(this.platformId) ? localStorage.getItem('token') : null);
+    return (
+      this.authToken ||
+      (isPlatformBrowser(this.platformId)
+        ? localStorage.getItem('token')
+        : null)
+    );
   }
 
   get currentUserId(): number | null {
@@ -42,18 +52,20 @@ export class AuthService {
   }
 
   login(credentials: any): Observable<any> {
-    return this.http.post<{ token: string }>(`${this.apiUrl}/login`, credentials).pipe(
-      tap(response => {
-        this.authToken = response.token;
-        if (isPlatformBrowser(this.platformId)) {
-          localStorage.setItem('token', response.token);
-        }
-        this._isAuthenticated = true;
-        const decodedToken: any = jwtDecode(response.token);
-        this.userId = decodedToken.sub;
-        console.log('User ID from token:', this.userId);
-      })
-    );
+    return this.http
+      .post<{ token: string }>(`${this.apiUrl}/login`, credentials)
+      .pipe(
+        tap((response) => {
+          this.authToken = response.token;
+          if (isPlatformBrowser(this.platformId)) {
+            localStorage.setItem('token', response.token);
+          }
+          this._isAuthenticated = true;
+          const decodedToken: any = jwtDecode(response.token);
+          this.userId = decodedToken.sub;
+          console.log('User ID from token:', this.userId);
+        })
+      );
   }
 
   logout(): void {
